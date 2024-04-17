942d6ce97460494f21acc3bb01f74e89b31e5a0e
Suad Suljovic <suad.suljovic92@gmail.com>
2024-04-17 21:08:19 +0200

    first commit

---
+++ cas1/intro.js
@@ -0,0 +1,9 @@
+// React, what is react?
+// https://react.dev
+// must read https://react.dev/learn/thinking-in-react
+
+// node, vite
+// How can we create a react project?
+
+// framework vs library
+// angular, vue, svelte, nextJS, react?
